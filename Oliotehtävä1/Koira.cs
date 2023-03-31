577e63fe5bf189c340ae2da602cd85c7f6172486
KasperVarje <kasper.varje@gmail.com>
2023-03-31 09:44:18 +0300

    Ajoneuvo Harjoitus1

---
+++ Oliotehtävä1/Koira.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus1
+{
+    internal class Ajoneuvo
+    {
+        public string nimi;
+        public int nopeus;
+        public int renkaat;
+
+        public string TulostaData()
+        {
+            string AjoneuvonTiedot = "nimi: " + nimi + " nopeus: " + nopeus.ToString() + " renkaat: " + renkaat.ToString();
+
+            return AjoneuvonTiedot;
+        }
+    }
+}
--- Oliotehtävä1/Program.cs
+++ Oliotehtävä1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Harjoitus1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Ajoneuvo Ajo = new Ajoneuvo();
+
+        Ajo.nimi = "Audi A4";
+        Ajo.nopeus = 230;
+        Ajo.renkaat = 4;
+
+        Console.WriteLine(Ajo.TulostaData());
+    }
+}
