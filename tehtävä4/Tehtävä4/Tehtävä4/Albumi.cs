3015ccc8384efd5947f05146f488e4d1f8d86960
KasperVarje <kasper.varje@gmail.com>
2023-03-31 12:04:10 +0300

    Tehtävä 4 valmis

---
+++ tehtävä4/Tehtävä4/Tehtävä4/Albumi.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtävä4
+{
+    internal class Albumi
+    {
+        public string Artisti;
+        public string Nimi;
+        public string Genre;
+        public int Kappalemäärä;
+        public int Hinta;
+
+        public List<string> kappale = new List<string>();
+
+        public void Kappalelisäys(string kappaleet)
+        {
+            kappale.Add(kappaleet);
+        }
+
+        public void albumitulostus()
+        {
+            Console.WriteLine("Albumi:");
+            Console.WriteLine("- Artisti: " + Artisti);
+            Console.WriteLine("- Albumin nimi: " + Nimi);
+            Console.WriteLine("- Gerne: " +  Genre);
+            Console.WriteLine("- Kappaleiden määrä: " +  Kappalemäärä);
+            Console.WriteLine("- Hinta: " + Hinta + " euroa");
+        }
+
+        public void biisitulostus()
+        {
+            Console.WriteLine("\n--- " + kappale[0] + " 3:15");
+            Console.WriteLine("--- " + kappale[1] + " 2:05");
+            Console.WriteLine("--- " + kappale[2] + " 2:43");
+            Console.WriteLine("--- " + kappale[3] + " 1:54");
+            Console.WriteLine("--- " + kappale[4] + " 3:03");
+        }
+
+    }
+}
---
+++ tehtävä4/Tehtävä4/Tehtävä4/Program.cs
@@ -0,0 +1,24 @@
+﻿using Tehtävä4;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Albumi albumi = new Albumi();
+        albumi.Artisti = "Pop Smoke";
+        albumi.Nimi = "Meet the Woo";
+        albumi.Genre = "Rap";
+        albumi.Kappalemäärä = 5;
+        albumi.Hinta = 5;
+
+
+        albumi.Kappalelisäys("Meet the Woo");
+        albumi.Kappalelisäys("Welcome to the party");
+        albumi.Kappalelisäys("Hawk Em");
+        albumi.Kappalelisäys("Scenario");
+        albumi.Kappalelisäys("Dior");
+
+        albumi.albumitulostus();
+        albumi.biisitulostus();
+    }
+}
