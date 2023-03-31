3e3457910b1cae4923968d3ce7d664542ed3f1a8
KasperVarje <kasper.varje@gmail.com>
2023-03-31 09:55:48 +0300

    Tehtävä 2 tehty push

--- Uusi kansio/Tehtävä2/ConsoleApp1/Program.cs
+++ Uusi kansio/Tehtävä2/ConsoleApp1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Harjoitus_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // luodaan Opiskelija olio
+            Opiskelija opiskelija1 = new Opiskelija("Kasper", "DATV22", 0);
+
+            opiskelija1.TulostaData();
+            opiskelija1.MuokkaaOpintopisteitä(5);
+            opiskelija1.TulostaData();
+        }
+    }
+}
