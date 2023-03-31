78dcd90e91a0234b9c58e8c9f00c7dca978972ab
KasperVarje <kasper.varje@gmail.com>
2023-03-31 12:33:58 +0300

    Tehtävä 5 valmis

---
+++ tehtävä5/Tehtävä5/Tehtävä5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Tehtävä5;
+
+namespace Tehtävä5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 2.0f;
+            float b = 1.5f;
+
+            float summa = Laskin.Summa(a, b);
+            float erotus = Laskin.Erotus(a, b);
+            float kertolasku = Laskin.Kertolasku(a, b);
+            float jakolasku = Laskin.Jakolasku(a, b);
+
+            Console.WriteLine("Numeroiden " + a + " ja " + b);
+            Console.WriteLine("Summa: " + summa);
+            Console.WriteLine("Erotus: " + erotus);
+            Console.WriteLine("Kertolasku: " + kertolasku);
+            Console.WriteLine("Jakolasku: " + jakolasku);
+        }
+    }
+}
