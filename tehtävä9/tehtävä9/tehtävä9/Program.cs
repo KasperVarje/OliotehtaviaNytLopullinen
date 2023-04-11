e2aebba29a335fcf005d5ca59f5748eeca7fe049
KasperVarje <kasper.varje@gmail.com>
2023-04-11 14:00:12 +0300

    teht9 keske

---
+++ tehtävä9/tehtävä9/tehtävä9/Program.cs
@@ -0,0 +1,12 @@
+﻿using tehtävä9;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Radio radio = new Radio();
+        radio.Kytkin();
+
+
+    }
+}
---
+++ tehtävä9/tehtävä9/tehtävä9/Radio.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tehtävä9
+{
+    internal class Radio
+    {
+        private bool Tila { get; set; }
+
+
+        public void Kytkin()
+        {
+            Console.WriteLine("Kytke kiuas päälle painamalla p ");
+            string tarkistus = Console.ReadLine();
+
+            if (tarkistus == null)
+            {
+                Console.WriteLine("Virhe joudut käynnistämään radion uudestaan!");
+            }
+            else if(tarkistus == "p")
+            {
+                Tila = true;
+                Console.WriteLine("Radio on kytketty päälle");      
+            }
+            else
+            {
+                Console.WriteLine("Virhe joudut käynnistämään radion uudestaan!");
+            }
+        }
+    }
+}
