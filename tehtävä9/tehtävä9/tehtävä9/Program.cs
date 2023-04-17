949bf70f6e34725c2583cf0f3f8243cb9bed11d6
KasperVarje <kasper.varje@gmail.com>
2023-04-17 12:53:51 +0300

    tehtävä 9 osa 1/2 tehty

--- tehtävä9/tehtävä9/tehtävä9/Program.cs
+++ tehtävä9/tehtävä9/tehtävä9/Program.cs
@@ -6,7 +6,37 @@ internal class Program
     {
         Radio radio = new Radio();
         radio.Kytkin();
+        radio.Taajuus1();
+        radio.Ääni1();
 
+        while (true)
+        {
+            Console.WriteLine("Syötä 'v' muuttaaksesi äänenvoimakkuutta, syötä 't' muuttaakksesi taajuutta, syötä 'c' katsoaksesi millä kanavalla olet, syötä 'p' sulkeaksesi radio.");
+            string tark = "";
+            tark = Console.ReadLine();
+            if(tark == "v")
+            {
+                radio.Ääni1();
+            }
+            else if(tark == "t")
+            {
+                radio.Taajuus1();
+            }
+            else if(tark == "p")
+            {
+                Console.WriteLine("Poistuit radiosta.");
+                break;
+            }
+            else if(tark == "c")
+            {
 
+            }
+            else
+            {
+                Console.WriteLine("Syötit väärän kirjaimen");
+                continue;
+            }
+
+        }
     }
 }
--- tehtävä9/tehtävä9/tehtävä9/Radio.cs
+++ tehtävä9/tehtävä9/tehtävä9/Radio.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,7 +14,7 @@ namespace tehtävä9
 
         public void Kytkin()
         {
-            Console.WriteLine("Kytke kiuas päälle painamalla p ");
+            Console.WriteLine("Kytke radio päälle painamalla p ");
             string tarkistus = Console.ReadLine();
 
             if (tarkistus == null)
@@ -29,6 +30,90 @@ namespace tehtävä9
             {
                 Console.WriteLine("Virhe joudut käynnistämään radion uudestaan!");
             }
+        }
+
+        public string TaajuusYksikkö = "MHz";
+
+        private readonly double Minimitaajuus = 88.0f;
+        private readonly double Maksimitaajuus = 107.9f;
+
+        private double taajuus;
+        public double Taajuus
+        {
+            get { return taajuus; }
+            set
+            {
+                if(value > Maksimitaajuus)
+                {
+                    value = Maksimitaajuus;
+                }
+                else if(value < Minimitaajuus)
+                {
+                    value = Minimitaajuus;
+                }
+                taajuus = value;
+            }
+        }
+
+        private readonly int MaxÄäni = 9;
+        private readonly int MinÄäni = 0;
+
+        private int ääni;
+
+        public int Ääni
+        {
+            get { return ääni; }
+            set
+            {
+                if(value < MinÄäni)
+                {
+                    value = MinÄäni;
+                }
+                else if(value > MaxÄäni)
+                {
+                    value = MaxÄäni;
+                }
+            }
+        }
+
+        public void Ääni1()
+        {
+            Console.WriteLine("Syötä äänen voimakkuus 0-9 väliltä.");
+            int Äänicheck = int.Parse(Console.ReadLine());
+            Ääni = Äänicheck;
+            if (Äänicheck < MinÄäni)
+            {
+                Console.WriteLine("Syöttämäsi äänenvoimakkuus alittaa minimi äänenvoimakkuuden arvoksi asetettu : " + MinÄäni.ToString());
+            }
+            else if (Äänicheck > MaxÄäni)
+            {
+                Console.WriteLine("Syöttämäsi äänenvoimakkuus ylittää maksimi äänenvoimakkuuden arvoksi asetettu : " + MaxÄäni.ToString());
+            }
+        }
+        public void Taajuus1()
+        {
+            Console.WriteLine("Syötä taajuus 88,0 - 107,9 väliltä");
+            double Taajuuscheck = double.Parse(Console.ReadLine());
+            Taajuus = Taajuuscheck;
+            if(Taajuuscheck < Minimitaajuus)
+            {
+                Console.WriteLine("Syöttämäsi taajuus oli pienenpi kuin minimi taajuus joten taajuudeksi asetettu : " + Minimitaajuus.ToString()
+                    + " " + TaajuusYksikkö);
+            }
+            else if (Taajuuscheck > Maksimitaajuus)
+            {
+                Console.WriteLine("Syöttämäsi taajuus oli isompi kuin maksimi taajuus joten taajuudeksi asetettu : " + Maksimitaajuus.ToString()
+                    + " " + TaajuusYksikkö);
+            }
+
+        }
+
+        private string tark;
+
+        public string Tark
+        {
+            get { return tark; } 
+            set { tark = value; }
         }
     }
 }
