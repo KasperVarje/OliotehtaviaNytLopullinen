2a20cc47b274c2b87d645e8920cc21c84422387a
KasperVarje <kasper.varje@gmail.com>
2023-04-18 10:18:51 +0300

    teht 9 osa 2 alotettu

---
+++ tehtävä9/tehtävä9/tehtävä9/Kanava.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tehtävä9
+{
+    internal class Kanava
+    {
+        private string Nimi { get; set; }
+        static double Taajuus1 { get; set; }
+
+        public void kanava(string _nimi, double _taajuus)
+        {
+            Nimi = _nimi;
+            Taajuus1 = _taajuus;
+        }
+
+        public double TaajuusP = Taajuus1;
+
+    }
+}
--- tehtävä9/tehtävä9/tehtävä9/Program.cs
+++ tehtävä9/tehtävä9/tehtävä9/Program.cs
@@ -9,25 +9,32 @@ internal class Program
         radio.Taajuus1();
         radio.Ääni1();
 
+        Kanava kanava1 = new Kanava();
+        kanava1.kanava("Moi", 88.2f);
+
+        if (radio.Taajuus == kanava1.TaajuusP)
+        {
+            Console.WriteLine("Toimii");
+        }
         while (true)
         {
             Console.WriteLine("Syötä 'v' muuttaaksesi äänenvoimakkuutta, syötä 't' muuttaakksesi taajuutta, syötä 'c' katsoaksesi millä kanavalla olet, syötä 'p' sulkeaksesi radio.");
             string tark = "";
             tark = Console.ReadLine();
-            if(tark == "v")
+            if (tark == "v")
             {
                 radio.Ääni1();
             }
-            else if(tark == "t")
+            else if (tark == "t")
             {
                 radio.Taajuus1();
             }
-            else if(tark == "p")
+            else if (tark == "p")
             {
                 Console.WriteLine("Poistuit radiosta.");
                 break;
             }
-            else if(tark == "c")
+            else if (tark == "c")
             {
 
             }
@@ -37,6 +44,12 @@ internal class Program
                 continue;
             }
 
+        }
+
+
+        if(radio.Taajuus == kanava1.TaajuusP)
+        {
+            Console.WriteLine("Toimii");
         }
     }
 }
