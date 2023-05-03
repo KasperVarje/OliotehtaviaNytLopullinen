eba6a563d5897e2f95103b29b5e1fa8acffd8a88
KasperVarje <kasper.varje@gmail.com>
2023-05-03 09:20:56 +0300

    joo

---
+++ teht10/t10 oikee/t10 oikee/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Harjoitus_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Varis varis = new Varis("Varis", 50);
+
+            varis.MakeASound();
+            varis.Fly();
+
+            Kala ahven = new Kala("Ahven", 30, "Blurps");
+
+            ahven.MakeASound();
+            ahven.Swim();
+
+            Sorsa sorsa = new Sorsa("Sorsa", 15, 10);
+
+            sorsa.MakeASound();
+            sorsa.Fly();
+            sorsa.MakeASound();
+            sorsa.Swim();
+        }
+    }
+}
